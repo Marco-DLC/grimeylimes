9e297e9872208769d932d64f01ff263c5502e380
Marco-DLC <140775824+Marco-DLC@users.noreply.github.com>
2024-04-19 14:20:40 -0400

    Link JSON data and add art containers by year
    Artwork will go in their respective containers by which year they were
    drawn.

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -1,4 +1,23 @@
-const container = document.querySelector('.outer-container').lastElementChild;
+const container = document.querySelector('#portfolioContainer');
 
+async function getArtData() {
+    const request = new Request('../art.json');
+    const response = await fetch(request);
+    const data = await response.json();
 
+    return data;
+}
 
+async function createTimeline() {
+    let art = await getArtData();
+    art.forEach((drawing) => {
+        if (!document.getElementById(drawing.year)) {
+            const yearContainer = document.createElement('div');
+            yearContainer.id = drawing.year;
+            yearContainer.innerHTML = `<h2>${drawing.year}</h2>`;
+            container.appendChild(yearContainer);
+        }
+    })
+}
+
+createTimeline();
