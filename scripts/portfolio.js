8a0d153f848c9d2244db80e71f79300787f9f611
Marco-DLC <140775824+Marco-DLC@users.noreply.github.com>
2024-05-05 01:36:40 -0400

    Add functions to filter by year and type/series
    The orderSort no longer works due to realizing it only reversed the
    order of the years, not the order of the individual art pieces in
    relevancy to date created. I will fix that next.

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -1,5 +1,29 @@
 const mainContentContainer = document.querySelector('#portfolioContainer');
-let defaultTimelineOrder = true;
+let reverseTimelineOrder = true;
+
+
+const yearFilterSel = document.getElementById('yearFilter');
+const mainFilterSel = document.getElementById('mainFilter');
+const seriesOptGroup = document.getElementById('seriesOpts');
+
+const orderSortBtn = document.getElementById('orderSortBtn');
+
+const galleryModal = document.querySelector('.gallery-modal');
+
+const monthsArray = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+];
 
 async function getArtData() {
     const request = new Request('../art.json');
@@ -10,7 +34,8 @@ async function getArtData() {
 }
 
 async function createTimeline() {
-    let artObjectArray = await getArtData();
+    artObjectArray = await getArtData();
+
     artObjectArray.forEach((artObj) => {
         if (!document.getElementById(artObj.year)) {
             const yearCreatedContainer = document.createElement('div');
@@ -28,6 +53,8 @@ function createArtGrids(artObjectArray) {
     artObjectArray.forEach((artObj) => {
         const yearCreatedContainer = document.getElementById(artObj.year);
         const artImage = document.createElement('img');
+        artImage.value = artObj.series;
+        artImage.classList.add('art', 'drawing');
         artImage.src = artObj.url || artObj.pages[0]; // If artObj is a collection of images (comic),
         // uses the first image of the comic, since the 'pages' property is an array of the comic's urls only existent for artObjs that are comics.
         artImage.oncontextmenu = function (event) {
@@ -49,7 +76,8 @@ function createArtGrids(artObjectArray) {
 
 function createComicContainer(artImage, artObj) {
     const comicContainer = document.createElement('div');
-    comicContainer.classList.add('comic-container');
+    comicContainer.classList.add('comic-container', 'art', 'comic');
+    comicContainer.value = artObj.series;
 
     const comicIconContainer = document.createElement('div');
     comicIconContainer.classList.add('comic-icon-container');
@@ -62,6 +90,7 @@ function createComicContainer(artImage, artObj) {
     comicIconContainer.appendChild(comicIcon);
     comicIcon.src = '../images/comic-icon.svg';
 
+    artImage.classList.remove('art');
     comicContainer.appendChild(artImage);
     comicContainer.appendChild(comicIconContainer);
 
@@ -85,7 +114,6 @@ function closeModal() {
     galleryModal.style.display = 'none';
 }
 
-const galleryModal = document.querySelector('.gallery-modal');
 galleryModal.querySelector('.close-modal-btn').addEventListener('click', () => closeModal());
 
 document.body.addEventListener('click', (e) => {
@@ -94,42 +122,33 @@ document.body.addEventListener('click', (e) => {
     }
 }, true);
 
-const monthsArray = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December'
-];
-
 function createFilter(artObjectArray) {
-    const filterContainer = document.getElementById('portfolioFilter');
-    const yearFilterSel = document.getElementById('yearFilter');
-    const mainFilterSel = document.getElementById('mainFilter');
-    const seriesOptGroup = document.getElementById('seriesOpts');
+    let yearIDs = [];
+    let seriesNames = [];
 
-    const orderSortBtn = document.getElementById('orderSortBtn');
     orderSortBtn.addEventListener('click', () => {
-        if (defaultTimelineOrder == true) {
+
+        if (reverseTimelineOrder == true) {
             orderSortBtn.innerHTML = 'Oldest <span class="arrow-up">&#10148;</span>';
-            mainContentContainer.style.flexDirection = 'column';
-            defaultTimelineOrder = false;
+            reverseTimelineOrder = false;
         } else {
             orderSortBtn.innerHTML = 'Newest <span class="arrow-down">&#10148;</span>';
-            mainContentContainer.style.flexDirection = 'column-reverse';
-            defaultTimelineOrder = true;
+            reverseTimelineOrder = true;
         }
     })
 
     mainContentContainer.childNodes.forEach((child) => {
-        if (child.id !== undefined && child.id !== '') {
+        let yearFilterExists = false;
+
+        for (let i = 0; i < yearFilterSel.children.length; i++) {
+            if (yearFilterSel.children[i].value === child.id) {
+                yearFilterExists = true;
+            }
+        }
+
+        if (child.id !== undefined && child.id !== '' && yearFilterExists == false) {
+            yearIDs.push(child.id);
+
             const option = document.createElement('option');
             option.value = child.id;
             option.innerHTML = child.id;
@@ -147,6 +166,8 @@ function createFilter(artObjectArray) {
         }
 
         if (seriesFilterExists == false) {
+            seriesNames.push(artObj.series);
+
             const option = document.createElement('option');
             option.value = artObj.series;
             option.innerHTML = artObj.series;
@@ -154,5 +175,41 @@ function createFilter(artObjectArray) {
         }
     })
 
+    yearFilterSel.addEventListener('change', () => filterArt(yearIDs, seriesNames));
+    mainFilterSel.addEventListener('change', () => filterArt(yearIDs, seriesNames));
+
 }
+
+function filterArt(allYears, allSeries) {
+    document.querySelectorAll('.art').forEach((art) => {
+        art.style.display = 'none';
+        if (mainFilterSel.value === art.value ||
+            art.classList.contains(mainFilterSel.value) ||
+            mainFilterSel.value === '') {
+            art.style.display = 'block';
+        }
+    })
+    allYears.forEach((year) => {
+        const yearCreatedContainer = document.getElementById(year);
+        if(!hasVisibleChildren(yearCreatedContainer) || yearFilterSel.value !== year) {
+            yearCreatedContainer.style.display = 'none';
+        } 
+
+        if (hasVisibleChildren(yearCreatedContainer) &&
+    (yearFilterSel.value === year || yearFilterSel.value === '')) {
+            yearCreatedContainer.style.display = 'grid';
+        }
+    })
+}
+
+function hasVisibleChildren(parent) {
+    const children = parent.children;
+    for (let i = 0; i < children.length; i++) {
+        if (window.getComputedStyle(children[i]).display !== 'none' && children[i].nodeName !== 'H2') {
+            return true;
+        }
+    }
+    return false;
+}
+
 createTimeline();
